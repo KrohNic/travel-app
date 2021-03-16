eda47d5b2de92254ed64f370d63a6e5a41aaeaf5
Kroh Nickolay <375336623761@ya.ru>
2021-03-16 23:10:40 +0300

    feat: add info to countries data

--- src/pages/CountryPage.js
+++ src/pages/CountryPage.js
@@ -4,7 +4,7 @@ import Header from '../components/Header/Header';
 import Wrapper from '../components/Wrapper/Wrapper';
 
 const CountryPage = (props) => {
-  const countryQuery = props.match.params.id || '';
+  // const countryQuery = props.match.params.id || '';
 
   console.log(props);
 
@@ -15,7 +15,16 @@ const CountryPage = (props) => {
   return (
     <React.Fragment>
       <Header />
-      <Wrapper>{countryQuery}</Wrapper>
+      <Wrapper>
+        <iframe
+          width='560'
+          height='315'
+          src='https://www.youtube-nocookie.com/embed/niO13sM0-Ew'
+          frameBorder='0'
+          allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
+          allowFullScreen
+        ></iframe>
+      </Wrapper>
     </React.Fragment>
   );
 };
